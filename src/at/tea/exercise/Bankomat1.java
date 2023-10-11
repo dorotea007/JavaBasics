a793f2f0ce456757f617f8129ebb234d186fe1d7
dorotea007 <dorotea.milic@student.htldornbirn.at>
2023-10-11 17:27:34 +0200

    Bankomat1_finished

--- src/at/tea/exercise/Bankomat1.java
+++ src/at/tea/exercise/Bankomat1.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Bankomat1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double kontostand = 1000.0;
+        double bankBalance = 1000.0;
 
         while (true) {
             // Bankomat Menü anzeigen
@@ -20,6 +20,38 @@ public class Bankomat1 {
             // Benutzereingabe lesen
             int option = scanner.nextInt();
 
+            switch (option) {
+                case 1:
+                    // Einzahlen
+                    System.out.print("Betrag zum Einzahlen eingeben: ");
+                    double depositAmount = scanner.nextDouble();
+                    bankBalance += depositAmount;
+                    System.out.println("Einzahlung erfolgreich.");
+                    break;
+                case 2:
+                    // Abheben
+                    System.out.print("Betrag zum Abheben eingeben: ");
+                    double withdrawAmount = scanner.nextDouble();
+                    if (withdrawAmount <= bankBalance) {
+                        bankBalance -= withdrawAmount;
+                        System.out.println("Abhebung erfolgreich.");
+                    } else {
+                        System.out.println("Nicht genügend Geld auf dem Konto.");
+                    }
+                    break;
+                case 3:
+                    // Kontostand abfragen
+                    System.out.println("Kontostand: " + bankBalance + " Euro");
+                    break;
+                case 4:
+                    // Beenden
+                    System.out.println("Vielen Dank für die Nutzung des Bankautomaten. Auf Wiedersehen!");
+                    scanner.close();
+                    System.exit(0);
+                default:
+                    System.out.println("Ungültige Option. Bitte wählen Sie erneut.");
+                    break;
+            }
         }
     }
 }
