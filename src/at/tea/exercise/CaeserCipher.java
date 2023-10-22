5be3c8f68ae93ef023895b744382abd78dd2d17b
dorotea007 <dorotea.milic@student.htldornbirn.at>
2023-10-22 16:46:41 +0200

    CaeserCipher_finished

---
+++ src/at/tea/exercise/CaeserCipher.java
@@ -0,0 +1,28 @@
+package at.tea.exercise;
+
+public class CaeserCipher {
+    public static String caesar(String text, int shift, boolean encrypt) {
+        StringBuilder result = new StringBuilder();
+        shift = encrypt ? shift : 26 - shift; // Für Entschlüsselung umkehren
+        for (char c : text.toCharArray()) {
+            if (Character.isLetter(c)) {
+                char base = Character.isUpperCase(c) ? 'A' : 'a';
+                char encryptedChar = (char) (base + (c - base + shift) % 26);
+                result.append(encryptedChar);
+            } else {
+                result.append(c);
+            }
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        String originalText = "AbC aBc";
+        int shift = 2;
+        String encryptedText = caesar(originalText, shift, true);
+        String decryptedText = caesar(encryptedText, shift, false);
+        System.out.println("Original: " + originalText);
+        System.out.println("Encrypted: " + encryptedText);
+        System.out.println("Decrypted: " + decryptedText);
+    }
+}
