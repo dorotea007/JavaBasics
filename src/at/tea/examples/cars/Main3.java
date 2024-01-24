2f4d4704b3ec8c19cfafaa86cd43606d6e89c7bf
dorotea007 <dorotea.milic@student.htldornbirn.at>
2024-01-24 09:04:15 +0100

    content changed in main_cars

--- src/at/tea/examples/cars/Main3.java
+++ src/at/tea/examples/cars/Main3.java
@@ -4,13 +4,13 @@ public class Main3 {
     public static void main(String[] args) {
 
 
-        Producer p1 = new Producer("McLaren", "UK", 0.3);
+        Producer p1 = new Producer("Lec", "Spain", 0.4);
 
         Engine e1 = new Engine(Engine.TYPE.DIESEL, 20);
-        Car c1 = new Car("red", 300, 300000, 10, p1, e1, 90000);
+        Car c1 = new Car("black", 400, 300000, 10, p1, e1, 90000);
 
-        System.out.println(c1.getPrice());
-        System.out.println(c1.getConsumption());
+        System.out.println("Price: " + c1.getPrice());
+        System.out.println("Consumption: " + c1.getConsumption());
 
     }
 }
